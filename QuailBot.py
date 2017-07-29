3a2b4df1dbb615dd80f603d0e4786910167ef312
Alice <aliceli.gm@gmail.com>
2017-07-28 22:31:09 -0500

    first commit, simple bot works

---
+++ QuailBot.py
@@ -0,0 +1,18 @@
+import discord
+import asyncio   # used for coroutines
+
+quailBot = discord.Client()  # instantiates discord.Client class to interact with Discord WebSocket and API
+
+@quailBot.event
+async def on_read():
+    print('Logged in as')
+    print(quailBot.user.name)
+    print(quailBot.user.id)
+    print('------')
+
+@quailBot.event
+async def on_message(message):
+    if message.content.startswith('!ping'):
+        await quailBot.send_message(message.channel, 'Pong!')
+
+quailBot.run('MzQwNjYzMDQ3MDAzNTcwMTc5.DF1zXQ.lWRRq23UGeamfgQE34Uy2oyHL_c')
